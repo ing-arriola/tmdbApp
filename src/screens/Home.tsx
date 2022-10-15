5dc26762f9a0632a55a935bff7f8d51905c69395
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-15 17:04:53 -0600

    Added home component to display popular movies

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,38 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { PopularMoviesResult, PupularMovies } from '../features/api/Apitypes';
+import { moviesApi } from '../features/api/LoginApi';
+import { MovieCard } from '../features/ui/MovieCard';
 export const Home = () => {
-  return <div>Home</div>;
+  const [popularMovies, setpopularMovies] = useState<PopularMoviesResult[]>([]);
+
+  const getPopularMovies = async () => {
+    try {
+      console.log(process.env.REACT_APP_MOVIE_APP);
+      const res = await moviesApi.get<PupularMovies>(
+        `/movie/popular?api_key=${process.env.REACT_APP_MOVIE_APP}`
+      );
+      setpopularMovies(res.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPopularMovies();
+  }, []);
+
+  return (
+    <MoviesContainer>
+      {popularMovies.map((popularMovie) => (
+        <MovieCard {...popularMovie} />
+      ))}
+    </MoviesContainer>
+  );
 };
+
+const MoviesContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 3fr));
+  gap: 8px;
+`;
