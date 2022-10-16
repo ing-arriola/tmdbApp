8dcc46c9871beb5bca0442de96ad507ed3ca77eb
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-16 12:26:54 -0600

    Added searchBar

--- src/features/ui/SearchBar.tsx
+++ src/features/ui/SearchBar.tsx
@@ -1,3 +1,40 @@
-export const SearchBar = () => {
-  return <div>SearchBar</div>;
+import { MouseEvent, Dispatch, SetStateAction } from 'react';
+import styled from 'styled-components';
+
+import { Button } from './Button';
+import { InputText } from './InputText';
+
+interface SearchBarProps {
+  movieToSearch: string;
+  setMovieToSearch: Dispatch<SetStateAction<string>>;
+  onSearchEvent: (event: MouseEvent<HTMLButtonElement>) => Promise<void>;
+}
+
+export const SearchBar = ({
+  movieToSearch,
+  setMovieToSearch,
+  onSearchEvent,
+}: SearchBarProps) => {
+  const onMovieSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMovieToSearch(event.target.value);
+  };
+
+  return (
+    <MovieSearchFormContainer>
+      <InputText
+        placeHolder="Search a movie"
+        value={movieToSearch}
+        setValue={onMovieSearchChange}
+        type="text"
+        name="movieToSearch"
+      />
+      <Button title="Search" onClick={onSearchEvent} />
+    </MovieSearchFormContainer>
+  );
 };
+
+const MovieSearchFormContainer = styled.form`
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  margin-bottom: 1rem;
+`;
