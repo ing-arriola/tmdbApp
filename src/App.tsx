74365995b5c639d9f0e680d0fae6dcf01644bf2f
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-18 13:04:55 -0600

    Added private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, Notfound, Details } from './screens/';
+import { PrivateRoutes } from './features/utils/PrivateRoutes';
+import { Login, Home, Notfound, Details } from './screens/';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path=":movieId" element={<Details />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path=":movieId" element={<Details />} />
+          </Route>
         </Route>
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </BrowserRouter>
--- src/features/utils/PrivateRoutes.tsx
+++ src/features/utils/PrivateRoutes.tsx
@@ -1,6 +1,6 @@
-import { Outlet, Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 export const PrivateRoutes = () => {
   let token = localStorage.getItem('token');
-  return token ? <Outlet /> : <Navigate to="/" />;
+  return token ? <Outlet /> : <Navigate to="/login" />;
 };
