1ca39668753eba22f6a20bf58cdb60af9e24ca16
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-18 11:37:45 -0600

    Added private routes component

---
+++ src/features/utils/PrivateRoutes.tsx
@@ -0,0 +1,6 @@
+import { Outlet, Navigate } from 'react-router-dom';
+
+export const PrivateRoutes = () => {
+  let token = localStorage.getItem('token');
+  return token ? <Outlet /> : <Navigate to="/" />;
+};
