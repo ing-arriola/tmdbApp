12f3602d5e6dff9afe6ecaa9112b2e326ac8c375
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-19 13:50:25 -0600

    Wrapped everthing in authContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { PrivateRoutes } from './features/utils/PrivateRoutes';
+import { AuthContextProvider } from './features/context/AuthContext';
+import { PrivateRoutes, PublicRoutes } from './features/utils/';
 import { Login, Home, Notfound, Details } from './screens/';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path="/home">
-            <Route index element={<Home />} />
-            <Route path="/home/:movieId" element={<Details />} />
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PublicRoutes />}>
+            <Route index element={<Login />} />
           </Route>
-        </Route>
-
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </BrowserRouter>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/home">
+              <Route index element={<Home />} />
+              <Route path="/home/:movieId" element={<Details />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 };
