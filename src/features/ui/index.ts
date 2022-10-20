0901d6ce4d1e3d270fbfbda60228f8ca569d2472
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-20 17:16:27 -0600

    updated imports

--- src/features/ui/index.ts
+++ src/features/ui/index.ts
@@ -1,4 +1,7 @@
+export { Loading } from './Loading';
+export { Carrousel } from './Carrousel';
 export { SearchBar } from './SearchBar';
 export { MovieCard } from './MovieCard';
 export {Button} from './Button'
 export { InputText } from './InputText'
+
